c96e8ad18e888e3b441a279a707cd52f68c9ed95
yuzu <takeuchiyuzuto0731@au.com>
2025-10-22 00:35:28 +0000

    save

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -5,21 +5,17 @@ function Intro() {
     const navigate = useNavigate();
     return (
         <div>
-            <head>
-            </head>
-            <body>
-                <nav>
-                    <ul>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                    </ul>
-                </nav>
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
             <h1>Welcome to MIMAMORI</h1>
             <p>Your safety is our priority. This application monitors your posture and alerts you in case of a fall.</p>
             <button onClick={() => navigate('/app')}>アプリ画面へ移動</button>
-            </body>
         </div>
     );
 }
